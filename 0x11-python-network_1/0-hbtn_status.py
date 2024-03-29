966d9733ff72c775934ca749b94702dd02e431a1
Profxbaba <daddyslove41@gmail.com>
2024-03-29 14:19:11 -0700

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script fetches the status of https://alx-intranet.hbtn.io/status
+and displays the body of the response in a specific format.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data.decode('utf-8')))
