4b3bb1096c68b105ba1cd65e9aacdcc0fbedf78d
Profxbaba <daddyslove41@gmail.com>
2024-03-29 11:00:38 -0700

    Python - Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input, sends a request to the URL using curl in silent mode, and displays the size of the response body in bytes
-
 curl -sI "$1" | awk '/Content-Length/ {print $2}' | tr -d '\r'
