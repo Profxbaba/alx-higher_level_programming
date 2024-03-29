49e814d53982d1ca9d8b0c106788f7f1e458327f
Profxbaba <daddyslove41@gmail.com>
2024-03-29 13:35:29 -0700

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the URL passed as the first argument with a header variable X-School-User-Id set to 98 and displays the body of the response
-
+# This script sends a GET request to the URL passed as the first argument with a header variable X-School-User-Id set to 98 & displays body of the response
 curl -sH "X-School-User-Id: 98" "$1"
